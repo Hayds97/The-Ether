4f30721e3044c9d129228025b9c207b4f7c4ebd7
Hayds97 <haydenvander@gmail.com>
2024-04-23 13:27:57 +0100

    creating connections and listners

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,1 +1,10 @@
+import express from 'express'
+
+const app = express(); 
+
+//middlewares
+app.use(express.json()); 
+
+//connection and listeners 
+app.listen(5000,()=>console.log("Server Open"));
 
