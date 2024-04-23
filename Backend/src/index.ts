533a0a88404e6cee6c779d147ccf1e45521eea6d
Hayds97 <haydenvander@gmail.com>
2024-04-23 20:43:59 +0100

    more Backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,9 +3,12 @@ import conncectToDatabase from "./db/connection.js";
 
 
 //connection and listeners 
+const PORT = process.env.PORT || 5000; 
 conncectToDatabase()
 .then(() => {
-  app.listen(5000, () =>
+  app.listen(PORT, () =>
     console.log("Server Open & Connected To The Database 🤟"));
 })
 .catch((err) => console.log(err)); 
+
+console.log ()
